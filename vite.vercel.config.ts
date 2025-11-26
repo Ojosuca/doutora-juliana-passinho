e26a30dfe28e12d876a8d0d77a273f091ec16cf0
Ojosuca <josueebonitobr@gmail.com>
2025-11-26 02:27:05 -0300

    Add Vercel build configuration

---
+++ vite.vercel.config.ts
@@ -0,0 +1,20 @@
+﻿import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: true,
+  },
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 5000,
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
